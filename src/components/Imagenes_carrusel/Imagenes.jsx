c28ff8cedb25af9baf6ddc578a58211b23d810a5
MarceloCariz <marcejedd300@hotmail.com>
2022-07-19 13:38:36 -0400

    new images

--- src/components/Imagenes_carrusel/Imagenes.jsx
+++ src/components/Imagenes_carrusel/Imagenes.jsx
@@ -7,12 +7,18 @@ import pa4 from '../../images/IMAGENES CARRUSEL/PA4.jpg'
 import pa5 from '../../images/IMAGENES CARRUSEL/PA5.jpg'
 import pa6 from '../../images/IMAGENES CARRUSEL/PA6.jpg'
 import pa7 from '../../images/IMAGENES CARRUSEL/PA7.jpg'
+import eb from '../../images/IMAGENES CARRUSEL/EquipoB.jpg'
+import ec from '../../images/IMAGENES CARRUSEL/EquipoC.jpg'
+import ede from '../../images/IMAGENES CARRUSEL/EquipoDE.jpg'
+import sala from '../../images/IMAGENES CARRUSEL/DondeEstaMiSala.jpg'
+
+
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 const Imagenes = () => {
 
   
   return (
-    (<Carousel className='centrar-carrusel'showStatus={false} infiniteLoop={true} showIndicators={false} showArrows={false} interval={6000} autoPlay={true} showThumbs={false}   >
+    (<Carousel className='centrar-carrusel'showStatus={false} infiniteLoop={true} showIndicators={false}  showArrows={false} interval={8000} autoPlay={true} showThumbs={false}   >
         <img src={pa1} alt="" />
         <img src={pa2} alt="" />
         <img src={pa3} alt="" />
@@ -20,6 +26,10 @@ const Imagenes = () => {
         <img src={pa5} alt="" />
         <img src={pa6} alt="" />
         <img src={pa7} alt="" />
+        <img src={eb} alt="" />
+        <img src={ec} alt="" />
+        <img src={ede} alt="" />
+        <img src={sala} alt="" />
     </Carousel>)
   )
 }
